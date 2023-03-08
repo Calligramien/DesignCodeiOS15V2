fc58bd01b3f4cadee84948783f5c650e19ab4a52
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2023-03-08 09:47:34 +0100

    Initial Commit

---
+++ DesignCodeiOS15V2/DesignCodeiOS15V2App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15V2App.swift
+//  DesignCodeiOS15V2
+//
+//  Created by Damien Gautier on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
