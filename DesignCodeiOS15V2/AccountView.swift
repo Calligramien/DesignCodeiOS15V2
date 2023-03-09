584ff4f1422bd7d160991286be4171c4ea7e547b
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2023-03-09 10:16:52 +0100

    [ADD] Creating simple navigation view layout + list styles

---
+++ DesignCodeiOS15V2/AccountView.swift
@@ -0,0 +1,36 @@
+//
+//  AccountView.swift
+//  DesignCodeiOS15V2
+//
+//  Created by Damien Gautier on 09/03/2023.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    Text("Settings")
+                    
+                    Text("Billing")
+                    
+                    Text("Help")
+                        
+                }
+                .listRowSeparatorTint(.blue)
+                .listRowSeparator(.hidden)
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("Account")
+        }
+        
+    }
+}
+
+struct AccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountView()
+    }
+}
