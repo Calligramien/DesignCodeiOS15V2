5ed7e0ecf75fba37614dd355580e7389ab720d73
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2023-03-15 12:17:07 +0100

    [ADD] Synchronizing non-persistent state across the app using environment object and Combine

---
+++ DesignCodeiOS15V2/Model/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  DesignCodeiOS15V2
+//
+//  Created by Damien Gautier on 15/03/2023.
+//
+
+import SwiftUI
+import Combine
+
+class Model: ObservableObject {
+    @Published var showDetail: Bool = false
+}
